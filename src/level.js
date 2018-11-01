968543d9ea5527e4707e04759a3049da6d56ea79
Konstantin Kovalev <constantiner@gmail.com>
2018-11-01 13:36:54 +0100

    Add levels constants

---
+++ src/level.js
@@ -0,0 +1,10 @@
+const ALL = Symbol.for("@constantiner/log4js.ALL");
+const TRACE = Symbol.for("@constantiner/log4js.TRACE");
+const DEBUG = Symbol.for("@constantiner/log4js.DEBUG");
+const INFO = Symbol.for("@constantiner/log4js.INFO");
+const WARN = Symbol.for("@constantiner/log4js.WARN");
+const ERROR = Symbol.for("@constantiner/log4js.ERROR");
+const FATAL = Symbol.for("@constantiner/log4js.FATAL");
+const OFF = Symbol.for("@constantiner/log4js.OFF");
+
+export { ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF };
