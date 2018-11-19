78f02d39929d8f83dea81216fe625fcf386ca010
Konstantin Kovalev <constantiner@gmail.com>
2018-11-19 17:44:25 +0100

    Refactoring to simplify code

--- src/logger.js
+++ src/logger.js
@@ -12,32 +12,49 @@ import { getCapitalizedMethodForSymbol, getMethodForSymbol, isValidLevel, validF
 const log = (level, category, ...args) => performLog(level, category, ...args);
 const isLevelEnabled = (level, category) => isRequestedLevelEnabled(level, category);
 
+const makeBaseLogger = category =>
+	validForLoggingSymbols.reduce((logger, level) => {
+		const methodName = getMethodForSymbol(level);
+		logger[methodName] = (...args) => log(level, category, ...args);
+		const capitalizedMethodName = getCapitalizedMethodForSymbol(level);
+		logger[`is${capitalizedMethodName}Enabled`] = () => isLevelEnabled(level, category);
+		return logger;
+	}, {});
+
+const configureSetAppender = (logger, category) => (
+	(logger.setAppender = function _setAppender(appender) {
+		setAppender(category, appender);
+		return this;
+	}.bind(logger)),
+	logger
+);
+
+const configureSetLevel = (logger, category) => (
+	(logger.setLevel = function _setLevel(level) {
+		if (!isValidLevel(level)) {
+			throw new Error(`Invalid level ${level}`);
+		}
+		setLevel(category, level);
+		return this;
+	}.bind(logger)),
+	logger
+);
+
+const createLogger = category => {
+	const logger = makeBaseLogger(category);
+	configureSetAppender(logger, category);
+	configureSetLevel(logger, category);
+	registerLogger(category, logger);
+	return logger;
+};
+
 const getLogger = category => {
 	const normalizedCategory = normalizeCategory(category);
 	const existingLogger = getExistingLogger(normalizedCategory);
 	if (existingLogger) {
 		return existingLogger;
 	}
-	const logger = validForLoggingSymbols.reduce((logger, level) => {
-		const methodName = getMethodForSymbol(level);
-		logger[methodName] = (...args) => log(level, normalizedCategory, ...args);
-		const capitalizedMethodName = getCapitalizedMethodForSymbol(level);
-		logger[`is${capitalizedMethodName}Enabled`] = () => isLevelEnabled(level, normalizedCategory);
-		return logger;
-	}, {});
-	logger.setAppender = function _setAppender(appender) {
-		setAppender(normalizedCategory, appender);
-		return this;
-	}.bind(logger);
-	logger.setLevel = function _setLevel(level) {
-		if (!isValidLevel(level)) {
-			throw new Error(`Invalid level ${level}`);
-		}
-		setLevel(normalizedCategory, level);
-		return this;
-	}.bind(logger);
-	registerLogger(normalizedCategory, logger);
-	return logger;
+	return createLogger(normalizedCategory);
 };
 
 const getRootLogger = () => getLogger();
